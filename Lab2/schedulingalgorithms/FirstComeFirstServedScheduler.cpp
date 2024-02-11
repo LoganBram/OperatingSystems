48373d45a0c5ea4e7d093e91bb7801eff3994169
KDalen <kai.michael.dalen@gmail.com>
2024-02-11 14:43:11 -0500

    1

--- Lab2/schedulingalgorithms/FirstComeFirstServedScheduler.cpp
+++ Lab2/schedulingalgorithms/FirstComeFirstServedScheduler.cpp
@@ -1,19 +1,45 @@
+#include <iostream>
 #include "FirstComeFirstServedScheduler.h"
 
 void FirstComeFirstServedScheduler::schedule()
 {
-    // Implementation of FCFS scheduling
-    throw "Not implemented"; // Remove this line
+    while (!processes.empty())
+    {
+        Process process = processes.front();
+        processes.pop();
+    }
 }
 
 void FirstComeFirstServedScheduler::calculateAverageWaitTime()
 {
+    int totalWaitTime = 0;
+    int count =0;
+    for (int i = 0; i < processes.size(); i++)
+    {
+        Process process = processes.front();
+        processes.pop();
+        totalWaitTime += process.waitTime;
+
+        count++;
+    }
+    double averageWaitTime = static_cast<double>(totalWaitTime) / count;
+    std::cout << "Average wait time: " << averageWaitTime << std::endl;
+    
     // Implementation of calculating average wait time
-    throw "Not implemented"; // Remove this line
 }
 
 void FirstComeFirstServedScheduler::calculateAverageTurnAroundTime()
 {
     // Implementation of calculating average turn around time
-    throw "Not implemented"; // Remove this line
+    int totalTurnAroundTime = 0;
+    int count =0;
+    for (int i = 0; i < processes.size(); i++)
+    {
+        Process process = processes.front();
+        processes.pop();
+        totalTurnAroundTime += process.waitTime + process.burstTime; //turnaround time = waiting time + burst time
+        count++;
+    }
+    double averageTurnAroundTime = static_cast<double>(totalTurnAroundTime) / count;
+    std::cout << "Average wait time: " << averageTurnAroundTime << std::endl;
 }
--- Lab2/schedulingalgorithms/FirstComeFirstServedScheduler.h
+++ Lab2/schedulingalgorithms/FirstComeFirstServedScheduler.h
@@ -8,8 +8,11 @@ using namespace std;
 
 class FirstComeFirstServedScheduler : public CPUScheduler
 {
+private:
+    std::queue<Process> processes; // Declare processes as a member variable
 public:
-    FirstComeFirstServedScheduler(queue<Process> processes, int quantum) {}
+    FirstComeFirstServedScheduler(queue<Process> processes, int quantum) {
+    }
     void schedule() override;
     void calculateAverageWaitTime() override;
     void calculateAverageTurnAroundTime() override;
